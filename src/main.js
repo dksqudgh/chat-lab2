c7f3f5b27dd95171fb7c51756e308b8e0be2e639
dksqudgh <dksqudgh7213@naver.com>
2025-05-29 16:10:38 +0900

    수정필요

---
+++ src/main.js
@@ -0,0 +1,185 @@
+//koa프레임워크가 해당 라이브러리를 찾을 수 있도록 선언함.
+//왜냐면 일을 시켜야 하니까
+const Koa = require('koa') //1
+//const app = new Koa();//2
+const path = require('path')//3
+const serve = require('koa-static')//6 - 정적리소스 경로 설정하기
+//15번처럼 js파일에 html태그를 작성하는 것은 너무 비효율적이다.
+//그래서 우리는 XXX.html문서 단위로 렌더링 처리를 요청할 수 있는
+//send라는 함수를 koa-send라이브러리로 부터 제공받는다.
+const send = require('koa-send')//7 - html파일을 통째로 렌더링 요청가능
+const mount = require('koa-mount') //8 - views와 public 구분
+const websockify = require('koa-websocket')//9 - 웹소켓
+const app = websockify(new Koa())//10 - 머지
+const route = require('koa-route')//11 - 요청을 구분 해서 처리
+const {getFirestore,collection, query, getDocs, addDoc} = require('firebase/firestore') //12 - 파이어스토어
+const { initializeApp } = require("firebase/app"); //로컬에서 참조함.
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBnbbKq3Sutrv-wp8yc46D9KSKHtB6KQ9s",
+  authDomain: "bookapp7213.firebaseapp.com",
+  databaseURL: "https://bookapp7213-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "bookapp7213",
+  storageBucket: "bookapp7213.firebasestorage.app",
+  messagingSenderId: "453880308052",
+  appId: "1:453880308052:web:501407a90fa77135ede8c1"
+};
+//firebase 앱 초기화
+const talkApp = initializeApp(firebaseConfig);
+const db = getFirestore(talkApp);
+//정적 리소스에 대한 파일 경로 설정하기
+const staticPath = path.join(__dirname, './views')//4
+
+app.use(serve(staticPath));//5
+
+//9 - public의 경로와 views의 경로에 같은 파일이 있으면 구별이 안된다.
+app.use(mount('/public', serve('src/public'))) //처리-이벤트- 말하기
+//서버는 5000번 포트를 열어놓고 기다린다. -waiting
+let time = '' //전변-다른 함수나  {} 안에서도 호출할 수 있다.
+const setClock = () => {
+  const timeInfo = new Date()
+  const hour = modifyNumber(timeInfo.getHours())
+  const min = modifyNumber(timeInfo.getMinutes())
+  const sec = modifyNumber(timeInfo.getSeconds())
+  time = hour+':'+min+':'+sec
+}
+
+const modifyNumber = (num) => {
+  if(parseInt(num) < 10){
+    return "0"+num
+  }else{
+    return num
+  }
+}
+//기본 라우터 설정하기
+app.use(async(ctx) => {
+  await setInterval(setClock, 1000)
+  if(ctx.path === '/'){
+    ctx.type = 'text/html'
+    //index.html문서가 하는 일을 여기에 작성해 본다.
+    ctx.body = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <title>welcome</title>
+      </head>
+      <body>
+        <h1>Welcome to the koa server</h1>
+      </body>
+      </html>    
+    `
+  }
+  //http://localhost:5000/talk
+  else if(ctx.path === '/login'){
+    await send(ctx, 'login.html', {root:staticPath})
+  }
+  else if(ctx.path === '/talk'){
+    await send(ctx, 'talk.html', {root:staticPath})
+  }
+  else if(ctx.path === '/notice'){
+    await send(ctx, 'notice.html', {root:staticPath})
+  }
+  else{
+    ctx.status = 404
+    ctx.body = 'Page Not Found'
+  }
+});//end of use
+
+//앞에 대화 내용을 가져오는 함수 배치하기
+const getChatsCollection = async() => {
+  //firestore api
+  const q = query(collection(db, 'talk250529'))
+  console.log(q);
+  const snapshot = await getDocs(q)
+  console.log(snapshot);
+  //data는 n건의 정보를 쥐고있다.. [{}, {}, ...]
+  //forEach를 사용해서 하나씩 꺼내서 처리할 수도 있지만
+  //map을 사용해서 배열로 변환할 수도 있다.
+  const data = snapshot.docs.map(doc => doc.data())
+  return data
+}//end of getChatsCollection
+
+
+// npm i koa-route 먼저 설치한다.
+// 왜나면 koa-websocket과 koa-route 서로 의존관계 있다.
+// Using routes
+app.ws.use(
+  //ws는 websocket을 의미한다.
+  //-> /test/:id로 요청이 오면 아래를 처리하라.
+  route.all('/ws', async(ctx) => {
+    const talks = getChatsCollection()
+    console.log(talks);
+    //console.log('talk : ' + talks);
+    ctx.websocket.send(JSON.stringify({
+      //클라이언트가 입장했을 때 sync인지 talk인지를 결정한다.- 서버
+      //그래서 서버가 결정해야 하므로 type에는 상수를 쓴다.
+      type:'sync',//firestore에서 가져온다.
+      payload: {
+        talks,//변수 - talks담긴 값은 어디서 가져오나요?
+      }
+    }))
+
+
+
+    const interval = setInterval(() => {
+      if(ctx.websocket.readyState === ctx.websocket.OPEN) {
+        //클라이언트가 연결되어 있으면
+        ctx.websocket.send(JSON.stringify({ 
+          message: 'ping' }  )); //ping 메시지를 보낸다.
+      }
+    },30000)
+
+  ctx.websocket.on('message', async(data) => {
+    //클라이언트가 보낸 메시지를 출력한다.
+    console.log(typeof data);
+    if(typeof JSON.stringify(data) !== 'string') {
+      return; //함수에서 return을 만나면 함수가 종료된다. //string이면 아래로
+    }
+
+    const { nickname, message } = JSON.parse(data);
+    console.log(`${nickname} , ${message} , ${time}`); //서버측 출력
+
+    try{
+      const docRef = await addDoc(collection(db, 'talk250529'), {
+        type: 'talk',
+        payload: {
+          nickname: nickname,
+          message: message,
+          time: time
+        }
+        //여기까지 진행이 되었다면
+      });
+      console.log('저장성공');
+    } catch{
+      console.error(error);
+    }
+    const { server } = app.ws;
+    if(!server) { 
+      return;//return을 만나면 use함수 전체를 빠져나감
+    }
+
+    server.clients.forEach((client) => {
+      //서버측에 연결된 모든 클라이언트에게 메시지를 전송한다.
+      if(client.readyState === client.OPEN){
+        client.send(JSON.stringify({
+          type: 'talk',
+          payload: {
+            nickname:nickname,
+            message: message,
+            time: time, //ES5
+          } 
+        })); 
+      }
+    });
+  });
+}));
+
+/* 
+//서버측에서 클라이언트측에 전송하기
+//들은것을 말하기
+ctx.websocket.send(JSON.stringify({ 
+  nickname: nickname,
+  message: message })); 
+*/
+
+app.listen(5000);
---
+++ src/public/client.js
@@ -0,0 +1,91 @@
+//alert('client.js loaded....')
+//브라우저 개발 도구에서 socket객체를 직접 호출하면 외부에 노출 위험이 있다.
+//즉시 실행 함수로 처리함. - IIFE - 바로 정의해서 호출하는 함수
+;(()=>{
+  let myNickname = prompt('닉네임을 입력하세요','default')
+  const title = document.querySelector('#title')
+  if(myNickname != null){
+    title.innerHTML = `{{${myNickname}}}님의 예약 상담`
+  }
+  const socket = new WebSocket(`ws://${window.location.host}/ws`)
+  const formEl = document.querySelector('#form')
+  const inputEl = document.querySelector('#input')
+  const chatsEl = document.querySelector('#chats')
+  
+  
+  
+  
+  
+  
+  if(!formEl || !inputEl || !chatsEl){ // | 나 || 결과는 같다 차이는 첫조건이 false일때 |는 뒤에도 확인하고 ||는 뒤에는 확인 안한다.
+    throw new Error('formEl or inputEl or chatsEl is null')
+  }
+  const chats = [] //서버에서 보내준 정보를 담는 배열이다. 청취한 정보가 담긴다.
+  //청취하기는 onmessage 이벤트 핸들러 처리한다.
+
+  //사용자가 입력한 메시지를 보내는 것은 submit에서 한다
+  formEl.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    //데이터를 직렬화 하는 방법은 여러가지가 있는데 가장 쉬운 방법이 JSON.stringify()를 사용하는 것이다.
+    //아래 send 함수는 string이나 버퍼류, Blob,등만 전달할 수 있다.
+    //그래서 문자열로 변환해서 전달해야 한다. JSON.stringify() , JSON.parse()
+    //데이터를 object로 직접 보낼 수가 없다.
+    //데이터를 소켓통신으로 전송하기 전에 JSON.stringify()로 감싸주는것 이것도 전처리 이다.
+    socket.send(JSON.stringify({ 
+      nickname: myNickname,
+      message: inputEl.value})) //서버측 출력
+      inputEl.value = '' //입력창 비우기(후처리)
+    })
+
+    
+    //화면과 로직은 분리한다.
+    const drawChats = () => {
+      chatsEl.innerHTML = ''; // 현재 대화 목록을 비운다
+      //div안에 새로운 div를 만들어서 채운다. <div><div>안쪽에 입력된다</div></div>
+      //[키위]:안녕(12:37:50)
+      chats.forEach(({message, nickname, time}) => {
+        const div = document.createElement('div'); //div를 생성한다.
+        div.innerText = `${nickname}: ${message} [${time}]`;
+        //바깥쪽 div에 안쪽 div를 추가한다. - appendChild
+        chatsEl.appendChild(div);
+      })
+    }//end of drawChats
+
+    //파라미터 자리는 사용자가 입력한 값을 담는 자리이다
+    //누가 넣어주나? 아래 이벤트는 소켓통신이 호출하는 콜백함수이다.
+    //콜백함수는 개발자가 직접 호출하는 것이 아니라 이벤트가 발생했을 때 시스템에서 자동으로 호출된다.
+    socket.addEventListener('message', (event)=> {
+      //아래 조건문에서 사용하는 type은 어디서 가져올까요
+      const { type,payload} = JSON.parse(event.data); //서버에서 보내준 데이터는 JSON.stringify()로 감싸져 있다.
+      /*
+      console.log(type);
+      console.log(payload);
+      console.log(payload.nickname);
+      console.log(payload.time);
+      console.log(payload.message);
+      */
+    
+      if('sync' === type){
+        const { talks:syncedChats } = payload;
+        Object.keys(syncedChats).map(key =>{
+          chats.push(syncedChats[key].payload)
+        })
+      }else if('talk' === type){
+        const talk = payload;
+        console.log(talk);
+        console.log(JSON.stringify(talk));  
+        chats.push(talk);
+        console.log(chats);
+      }
+      
+      chats.push(JSON.parse(event.data))//청취한 메시지를 배열에 담는다.
+      chatsEl.innerHTML = '' //화면 초기화
+      chats.forEach(({nickname, message}) => {//배열에 담긴 여러 메시지를 출력한다
+        const div = document.createElement('div')
+        div.innerText = `${nickname}: ${message}[12:34]`
+        chatsEl.appendChild(div)
+      })//end of event listener
+    })
+    drawChats();
+})()
+
